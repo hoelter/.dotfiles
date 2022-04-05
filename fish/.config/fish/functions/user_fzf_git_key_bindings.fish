4693b57bb60e193780dff1012ffac34cf797e64f
Christopher Hoelter <git@christopherhoelter.com>
2022-04-04 19:42:16 -0500

    Modified git fzf script and install setting

--- fish/.config/fish/functions/user_fzf_git_key_bindings.fish
+++ fish/.config/fish/functions/user_fzf_git_key_bindings.fish
@@ -1,10 +1,10 @@
 function user_fzf_git_key_bindings
 
   function fzf-select-branch-name -d "Select git branch name"
-    git for-each-ref --sort=-committerdate --color=always --format=' %(color:yellow)%(committerdate:short) %(committerdate:relative) %(color:red)%(refname:short) %(color:cyan)%(authorname) %(color:reset)%(subject)' | fzf-tmux -p --reverse | rev | cut -d " " -f 1 | cut -d "/" -f 1 | rev | read -lz result
+    git for-each-ref --sort=-committerdate --color=always --format='%(color:red)%(refname:short) %(color:yellow)%(committerdate:short) %(committerdate:relative) %(color:cyan)%(authorname) %(color:reset)%(subject)' | fzf-tmux -p  95%,80% --reverse | cut -d " " -f 1 | rev | cut -d "/" -f 1 | rev | xargs | read -l result
     and commandline -it -- $result
     commandline -f repaint
   end
 
-  bind -M insert \cg fzf-select-branch-name
+  bind -M insert \cgb fzf-select-branch-name
 end
