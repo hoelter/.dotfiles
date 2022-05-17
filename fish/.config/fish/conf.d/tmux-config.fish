8c0a4208eeedd6cdc6e9772e902352cfe659e4a1
Christopher Hoelter <git@christopherhoelter.com>
2022-05-17 09:20:31 -0500

    Updated tmux running check.

--- fish/.config/fish/conf.d/tmux-config.fish
+++ fish/.config/fish/conf.d/tmux-config.fish
@@ -1,6 +1,6 @@
 if status --is-interactive
-    if not set -q TMUX_FISH_BINDS
-        # set -U TMUX_FISH_BINDS true
+    set tmux_running (pgrep tmux)
+    if test -n "$tmux_running"
         # Launch custom rg finder
         tmux unbind-key -n M-r
         tmux bind-key -n M-r display-popup -h 95% -w 95% -E "rg-fzf"
@@ -8,8 +8,6 @@ if status --is-interactive
         # Websearch script
         tmux unbind-key -T root M-f
         tmux bind -T root M-f switch-client -T websearch_table
-
-        tmux unbind-key -T websearch_table g
         tmux bind-key -T websearch_table g display-popup -h 15% -E "websearch google 'https://www.google.com/search?q'"
 
         # Shortcuts
