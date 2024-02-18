0c631228eb939819c5c4b0199e9cd1689de74922
Christopher Hoelter <git@christopherhoelter.com>
2024-02-17 19:33:47 -0600

    Quick commit

---
+++ non-stowed-setup-scripts/copyinstall-i3-configs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo 'Starting copying of files for i3 setup'
+
+sudo cp ~/.dotfiles/non-stowed-copied-files/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/.
+
+echo 'Completed copying of files for i3 setup'
