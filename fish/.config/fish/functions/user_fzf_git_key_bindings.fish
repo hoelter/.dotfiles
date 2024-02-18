abd3fedba917401a060149105b0862836e8367dd
Christopher Hoelter <git@christopherhoelter.com>
2024-02-18 17:54:24 -0600

    Changing fzf-tmux to fzf.

--- fish/.config/fish/functions/user_fzf_git_key_bindings.fish
+++ fish/.config/fish/functions/user_fzf_git_key_bindings.fish
@@ -1,17 +1,17 @@
 function user_fzf_git_key_bindings
     function fzf-select-branch-name -d "Select git branch name"
-        git for-each-ref --sort=-committerdate --color=always --format='%(color:red bold)%(refname:short) %(color:reset)%(color:blue)%(committerdate:short) %(committerdate:relative) %(color:green)%(authorname) %(color:reset)%(subject)' | fzf-tmux -d 100% --reverse --no-sort | cut -d " " -f 1 | rev | cut -d "/" -f 1 | rev | xargs | read -l result
+        git for-each-ref --sort=-committerdate --color=always --format='%(color:red bold)%(refname:short) %(color:reset)%(color:blue)%(committerdate:short) %(committerdate:relative) %(color:green)%(authorname) %(color:reset)%(subject)' | fzf --reverse --no-sort | cut -d " " -f 1 | rev | cut -d "/" -f 1 | rev | xargs | read -l result
         and commandline -it -- $result
         commandline -f repaint
     end
 
     function fzf-select-commit-hash -d "Select git commit hash"
         if set -q MACBOOK
-            git l | fzf-tmux -d 100% --reverse --no-sort | gsed 's/.*\*[ |]*\(\S*\).*/\1/' | xargs | read -l result
+            git l | fzf --reverse --no-sort | gsed 's/.*\*[ |]*\(\S*\).*/\1/' | xargs | read -l result
             and commandline -it -- $result
             commandline -f repaint
         else
-            git l | fzf-tmux -d 100% --reverse --no-sort | sed 's/.*\*[ |]*\(\S*\).*/\1/' | xargs | read -l result
+            git l | fzf --reverse --no-sort | sed 's/.*\*[ |]*\(\S*\).*/\1/' | xargs | read -l result
             and commandline -it -- $result
             commandline -f repaint
         end
