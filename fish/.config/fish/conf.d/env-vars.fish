52f9960c7eaae6b856b0ce7fc211ad5fbd36df07
Christopher Hoelter <git@christopherhoelter.com>
2024-02-19 22:02:26 -0600

    Quick commit

--- fish/.config/fish/conf.d/env-vars.fish
+++ fish/.config/fish/conf.d/env-vars.fish
@@ -19,7 +19,7 @@ if status --is-interactive
     
     ## Init FZF settings
     set -gx FZF_DEFAULT_COMMAND "fd --type file --hidden --follow --strip-cwd-prefix --ignore-file $HOME/.config/fd/ignore"
-    set -gx FZF_DEFAULT_OPTS '--ansi --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#A3BE8C,fg+:#eceff4,prompt:#81A1C1,hl+:#81A1C1'
+    set -gx FZF_DEFAULT_OPTS '--ansi --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#4C566A,fg:#D8DEE9,header:#4C566A,info:#81A1C1,pointer:#81A1C1,marker:#A3BE8C,fg+:#eceff4,prompt:#81A1C1,hl+:#81A1C1'
     set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
     set -gx FZF_ALT_C_COMMAND "fd --type directory --hidden --follow --strip-cwd-prefix --ignore-file $HOME/.config/fd/ignore"
     set -gx FZF_ALT_C_OPTS "--ansi"
