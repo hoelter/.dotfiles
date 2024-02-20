1d4aae2366c86ea609bac42adff25f3b55806677
Christopher Hoelter <git@christopherhoelter.com>
2024-02-20 11:17:17 -0600

    Quick commit

--- non-stowed-setup-scripts/install-macbook.sh
+++ non-stowed-setup-scripts/install-macbook.sh
@@ -6,6 +6,9 @@ fish -c 'set -U MACBOOK true'
 
 ./update-zshrc
 
+# echo "Creating podman to docker alias"
+# ln -s /opt/homebrew/bin/podman ~/.local/bin/docker
+
 ./install fd fish git tmux vim kitty scripts lf javascript alacritty
 
 cd ~/.dotfiles-personal 2> /dev/null && ./install-macbook.sh
