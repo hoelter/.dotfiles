ac8765da571d0ebb87d3bc885cfd8f5c019f4a87
Christopher Hoelter <git@christopherhoelter.com>
2024-02-20 00:39:57 -0600

    Quick commit

--- non-stowed-setup-scripts/copyinstall-i3-configs.sh
+++ non-stowed-setup-scripts/copyinstall-i3-configs.sh
@@ -2,7 +2,7 @@
 
 echo 'Starting copying of files for i3 setup'
 
-sudo cp ~/.dotfiles/non-stowed-copied-files/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/.
-sudo cp ~/.dotfiles/non-stowed-copied-files/50-enable-Cozette.conf /etc/fonts/conf.avail/.
+sudo cp -n ~/.dotfiles/non-stowed-copied-files/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/. || true
+# sudo cp ~/.dotfiles/non-stowed-copied-files/50-enable-Cozette.conf /etc/fonts/conf.avail/.
 
 echo 'Completed copying of files for i3 setup'
--- non-stowed-setup-scripts/install-desktop.sh
+++ non-stowed-setup-scripts/install-desktop.sh
@@ -6,9 +6,9 @@ echo 'Debian dotfiles install initiated.'
 
 ./install fd fish git tmux vim scripts lf javascript i3 fonts
 
-cd ~/.dotfiles-personal 2> /dev/null && ./install-desktop.sh
-
 ./copyinstall-i3-configs.sh
+
+cd ~/.dotfiles-personal 2> /dev/null && ./install-desktop.sh
 
 echo 'Debian dotfiles install complete.'
 
