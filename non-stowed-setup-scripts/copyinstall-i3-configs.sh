7a2fc33d8e3dd1a377fc27c530b94879872c5a95
Christopher Hoelter <git@christopherhoelter.com>
2024-05-26 12:21:42 -0500

    Quick commit

--- non-stowed-setup-scripts/copyinstall-i3-configs.sh
+++ non-stowed-setup-scripts/copyinstall-i3-configs.sh
@@ -3,6 +3,8 @@
 echo 'Starting copying of files for i3 setup'
 
 sudo cp -n ~/.dotfiles/non-stowed-copied-files/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/. || true
-sudo cp ~/.dotfiles/non-stowed-copied-files/30bpp-displayp3colorsupport.conf /etc/X11/xorg.conf.d/. || true
+
+# Had issue with neovim xterm colors
+# sudo cp ~/.dotfiles/non-stowed-copied-files/30bpp-displayp3colorsupport.conf /etc/X11/xorg.conf.d/. || true
 
 echo 'Completed copying of files for i3 setup'
