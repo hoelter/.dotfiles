808f2db5bec942ad5ce6ed6b3a985857e207153a
Christopher Hoelter <git@christopherhoelter.com>
2024-05-26 12:04:03 -0500

    Added 30bpp xorg file

--- non-stowed-setup-scripts/copyinstall-i3-configs.sh
+++ non-stowed-setup-scripts/copyinstall-i3-configs.sh
@@ -3,6 +3,6 @@
 echo 'Starting copying of files for i3 setup'
 
 sudo cp -n ~/.dotfiles/non-stowed-copied-files/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/. || true
-# sudo cp ~/.dotfiles/non-stowed-copied-files/50-enable-Cozette.conf /etc/fonts/conf.avail/.
+sudo cp ~/.dotfiles/non-stowed-copied-files/30bpp-displayp3colorsupport.conf /etc/X11/xorg.conf.d/. || true
 
 echo 'Completed copying of files for i3 setup'
