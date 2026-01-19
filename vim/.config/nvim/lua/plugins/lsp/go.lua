bc620e65990f34b05233fb952b75d42b56cebec2
Christopher Hoelter <git@christopherhoelter.com>
2026-01-18 22:01:19 -0600

    Quick commit

--- vim/.config/nvim/lua/plugins/lsp/go.lua
+++ vim/.config/nvim/lua/plugins/lsp/go.lua
@@ -5,7 +5,20 @@ return {
   servers = { "gopls" },
   setup = function()
     vim.lsp.config('gopls', {
-      on_attach = base.on_attach_lsp,
+      on_attach = function(client, bufnr)
+        base.on_attach_lsp(client, bufnr)
+
+        -- Go-specific: <leader>f formats AND organizes imports
+        vim.keymap.set('n', '<leader>f', function()
+          -- Organize imports first
+          vim.lsp.buf.code_action({
+            context = { only = { "source.organizeImports" } },
+            apply = true,
+          })
+          -- Then format
+          vim.lsp.buf.format()
+        end, { buffer = bufnr, desc = "Format and organize imports" })
+      end,
       flags = base.common_flags,
       capabilities = base.get_capabilities()
     })
