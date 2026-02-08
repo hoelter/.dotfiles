69e1cd2fc7a28dc28d77355aa19a3495886a58f1
Christopher Hoelter <git@christopherhoelter.com>
2026-02-08 15:40:28 -0600

    Added installer for roslyn and razor

--- vim/.config/nvim/lua/plugins/lsp/csharp.lua
+++ vim/.config/nvim/lua/plugins/lsp/csharp.lua
@@ -1,27 +1,45 @@
 -- C# LSP configuration
 local base = require('plugins.lsp._base')
 
+local roslyn_dir = vim.fn.expand("~/.lsp/roslyn")
+local razor_dir = vim.fs.joinpath(roslyn_dir, ".razorExtension")
+
 return {
   servers = {},  -- Roslyn doesn't use mason
   setup = function()
     -- Plugin setup (no special options needed)
     require("roslyn").setup()
 
+    local cmd = {
+      "dotnet",
+      vim.fs.joinpath(roslyn_dir, "Microsoft.CodeAnalysis.LanguageServer.dll"),
+      "--logLevel=Information",
+      "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+      "--stdio",
+    }
+
+    -- Add razor extension flags if the extension is installed
+    if vim.fn.isdirectory(razor_dir) == 1 then
+      vim.list_extend(cmd, {
+        "--razorSourceGenerator="
+          .. vim.fs.joinpath(razor_dir, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+        "--razorDesignTimePath="
+          .. vim.fs.joinpath(razor_dir, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+        "--extension",
+        vim.fs.joinpath(razor_dir, "Microsoft.VisualStudioCode.RazorExtension.dll"),
+      })
+    end
+
     -- LSP configuration with custom cmd path
     vim.lsp.config("roslyn", {
-      cmd = {
-        "dotnet",
-        vim.fn.expand("~/.lsp/csharp-lsp/content/LanguageServer/osx-arm64/Microsoft.CodeAnalysis.LanguageServer.dll"),
-        "--logLevel=information",
-        "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-        "--stdio"
-      },
+      cmd = cmd,
+      filetypes = { "cs", "razor" },
       on_attach = base.on_attach_lsp,
       capabilities = base.get_capabilities(),
       settings = {
         razor = {
           language_server = {
-            cohosting_enabled = false,
+            cohosting_enabled = true,
           },
         },
       },
